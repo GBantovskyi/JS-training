6eb0e33be8689f537121d8b28b853078dd31bd5e
Grigorii Bantovskyi <gbantovsky@gmail.com>
2019-02-01 16:24:01 +0000

    js training homework

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const a = parseFloat(prompt('enter a'));
+const b = parseFloat(prompt('enter b'));
+const c = parseFloat(prompt('enter c'));
+
+const result = solveQuardEq(a, b, c);
+alert (result);
+
+function solveQuadrEq(a, b, c) {
+const discrim = (b^2-4*a*c);
+const xOne = (-b+math.sqrt(discrim))/2*a; 
+const xTwo = (-b-math.sqrt(discrim))/2*a;
+return 'x1'= xOne, 'x2'= xTwo; 
+
+
+
+

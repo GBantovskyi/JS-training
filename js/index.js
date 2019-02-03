976fceb64311ef94700f89936726f4b77c8e75d6
Grigorii Bantovskyi <gbantovsky@gmail.com>
2019-02-03 16:37:12 +0000

    js training math

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
-const a = parseFloat(prompt('enter a'));
-const b = parseFloat(prompt('enter b'));
-const c = parseFloat(prompt('enter c'));
-
-const result = solveQuardEq(a, b, c);
-alert (result);
-
-function solveQuadrEq(a, b, c) {
-const discrim = (b^2-4*a*c);
-const xOne = (-b+math.sqrt(discrim))/2*a; 
-const xTwo = (-b-math.sqrt(discrim))/2*a;
-return 'x1'= xOne, 'x2'= xTwo; 
+const a = parseFloat(prompt("Enter a"));
+const b = parseFloat(prompt("Enter b"));
+const c = parseFloat(prompt("Enter c"));
 
 
 
+const result = calculateQdE(a, b, c);
+alert(result);
 
+function calculateQdE(a, b, c) {
+
+    const discrim = b * b - 4 * a * c;
+    const xOne = (-b+Math.sqrt(discrim))/(2*a);
+    const xTwo = (-b-Math.sqrt(discrim))/(2*a);
+    if (discrim > 0) {
+    return ["X1= " + xOne + ", X2= " + xTwo];
+}else{
+    return "D <= 0"
+}
+}

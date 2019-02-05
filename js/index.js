1853886d5e62130ccffdd04b25383f6fea7c94f9
Grigorii Bantovskyi <gbantovsky@gmail.com>
2019-02-05 14:58:11 +0000

    Quadratic equation

--- js/index.js
+++ js/index.js
@@ -2,19 +2,19 @@ const a = parseFloat(prompt("Enter a"));
 const b = parseFloat(prompt("Enter b"));
 const c = parseFloat(prompt("Enter c"));
 
+const discriminant = b*b-4*a*c;
+const xOne = (-b+Math.sqrt(discriminant))/(2*a);
+const xTwo = (-b-Math.sqrt(discriminant))/(2*a);
 
-
-const result = calculateQdE(a, b, c);
+const result = calculateQuadraticEquation(a, b, c);
 alert(result);
 
-function calculateQdE(a, b, c) {
+function calculateQuadraticEquation(a, b, c) {
+    if (discriminant > 0) {
+        return ["X1 = " + xOne + " and X2 = " + xTwo]
+    } else {
+        return "discriminant equal or less then zero";
+    }
+}
 
-    const discrim = b * b - 4 * a * c;
-    const xOne = (-b+Math.sqrt(discrim))/(2*a);
-    const xTwo = (-b-Math.sqrt(discrim))/(2*a);
-    if (discrim > 0) {
-    return ["X1= " + xOne + ", X2= " + xTwo];
-}else{
-    return "D <= 0"
-}
-}
+
